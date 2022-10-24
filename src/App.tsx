474938babd57e9894fb16fdff21230283de4d56e
tochibedford <musicofbyte@gmail.com>
2022-10-24 10:43:53 +0100

    feat: implemented links section and link schema

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,21 @@ import shareIcon from './assets/shareIcon.svg'
 import menuIcon from './assets/menuIcon.svg'
 
 function App() {
+
+  type TuserLinks = {
+    id: string,
+    url: string,
+    text: string
+  }[]
+
+  // using an array to hold links will make it easier to change things or replace with an api Call in the future
+  const userLinks: TuserLinks = [
+    {id: "btn", url: "https://training.zuri.team/", text: "Zuri Team"},
+    {id: "books", url: "https://training.zuri.team/", text: "Zuri Books"},
+    {id: "book__python", url: "https://books.zuri.team/python-for-beginners?ref_id=tochibedford", text: "Python Books"},
+    {id: "pitch", url: "https://background.zuri.team", text: "Background Check for Coders"},
+    {id: "book__design", url: "https://background.zuri.team", text: "Design Books"},
+  ]
   return (
     <div className="App">
       <header className="profile">
@@ -19,6 +34,11 @@ function App() {
           <img src={menuIcon} alt="menu button" className="menu__icon" />
         </div>
       </header>
+      <main className="links">
+        {userLinks.map((link, index)=>{
+          return <a key={`${link.id + index}`} id={link.id} className="link__button" href={link.url}>{link.text}</a>
+        })}
+      </main>
     </div>
   )
 }
