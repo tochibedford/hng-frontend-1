c9512c7deddce3e24c3513d2a55bd399488d335a
tochibedford <musicofbyte@gmail.com>
2022-11-04 15:30:04 +0100

    feat: implemented heading section

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,6 +1,12 @@
+import styles from '../../styles/Contact.module.scss'
+
 export default function Contact() {
     return (
         <>
+            <header role="heading" className={styles.page__title__container}>
+                <h1 className={styles.page__title}>Contact Me</h1>
+                <h2 className={styles.page__subtitle}>Hi there, contact me to ask me about anything you have in mind.</h2>
+            </header>
         </>
     )
 }
